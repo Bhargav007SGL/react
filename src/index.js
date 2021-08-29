c18fd6c8ad92988ac3345d113f6c4e05fb2b50b7
Bhargav007SGL <bhargav.v@sgligis.com>
2021-08-29 17:37:13 +0530

    add my name in it

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function First()
+{
+  return <h1>Hello Bhargav Vanra</h1>;
+}
+
+
+ReactDom.render(<First/>,document.getElementById('root'));
